ad28decb487d81b9c4350174ae0db9cf1ef98818
sjk <sjk@renocomms.com>
2023-04-05 11:43:00 +0900

    vscode

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import NavBar from "./componets/NavBar";
 export default function Home() {
     return (
         <div>
-            <h1>hello1000</h1>
+            <h1>hello100000</h1>
         </div>
     );
 }
