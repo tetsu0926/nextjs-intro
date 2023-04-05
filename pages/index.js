0f8c05a7aa8d10e3c7c208a119d1364f490aa326
sjk <sjk@renocomms.com>
2023-04-05 12:30:13 +0900

    VS

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import NavBar from "./componets/NavBar";
 export default function Home() {
     return (
         <div>
-            <h1>hello1000_IJ</h1>
             <h1>hello1000_VSCODE1</h1>
         </div>
     );
