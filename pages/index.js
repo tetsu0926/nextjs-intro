2e8d34b90f89a2e3c50077eceb1187324428cf27
sjk <sjk@renocomms.com>
2023-04-05 13:41:51 +0900

    VS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./componets/NavBar";
 
 export default function Home() {
     const [counter, setCounter] = useState(0);
+
+    useEffect(() => {});
 
     return (
         <div>
