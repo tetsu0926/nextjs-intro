f2b61bc558676962fe0e1b73934e8802c5b0e340
sjk <sjk@renocomms.com>
2023-04-05 12:44:12 +0900

    vscode

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ export default function Home() {
     return (
         <div>
             <h1>hello1000_vscode</h1>
+            <h1>hello1000_IJ</h1>
+            <h1>hello1000_IJ</h1>
+            <h1>hello1000_IJ</h1>
         </div>
     );
 }
