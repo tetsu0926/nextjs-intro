9601aaca1b76916d9609aaed2ad3aa7d5d92a23c
sjk <sjk@renocomms.com>
2023-04-05 13:50:59 +0900

    vs

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,10 @@ export default function Home() {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {});
-     const test = 1;
-
-    const test = "test";
 
     return (
         <div>
-            <h1>hello1000_IJ</h1>
+            <h1>hello</h1>
         </div>
     );
 }
